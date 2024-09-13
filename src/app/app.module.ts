4225dc7adfe4ef7d801145d546a63402030e98f6
someshjagtap <someshjagtap12@gmail.com>
2024-09-14 00:07:38 +0530

    Add CalculatorComponent to AppModule and include FormsModule
    - Declared `CalculatorComponent` in `AppModule` for inclusion in the application.
    - Imported `FormsModule` to support two-way data binding in the calculator component.
    - Updated `AppModule` to bootstrap with `AppComponent`.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
